d5f93b76502f154e3babdfef20dee876cc714595
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-11-05 17:28:32 -0500

    changed variables

--- main.ts
+++ main.ts
@@ -5,27 +5,45 @@
  * This program checks how bright the light is
 */
 
-let lightLevel = 0
-let strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
+//variables
+let strip = robotbit.rgb() 
 
+//setup
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
 
+//When it is on
 basic.forever(function() {
-
-lightLevel = input.lightLevel()
-
-    // Turn off all NeoPixels
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-
-    if (lightLevel > 51) {
+    let lightLevel = input.lightLevel()
+    if (lightLevel <= 51) {
+        strip.clear() 
+    }
+    if (lightLevel > 52) {
+        strip.clear()
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
     }
     if (lightLevel > 104) {
+        strip.clear()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red)) 
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
     }
     if (lightLevel > 156) {
+        strip.clear()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red)) 
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
         strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
     }
     if (lightLevel > 208) {
+        strip.clear()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red)) 
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
         strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
     }
+    // Add a short delay
+    basic.pause(100)
 })
